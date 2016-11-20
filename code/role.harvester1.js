d3b5a64cd4eca527a3b199f3d9f1a769ecacf30a
Zyrax <h00davhe@gmail.com>
2016-11-20 23:39:00 +0100

    commit didnt work?

--- code/role.harvester1.js
+++ code/role.harvester1.js
@@ -2,8 +2,10 @@ var roleHarvester1 = {
 
     run: function(creep) {
         
-        if (creep.carry.energy == 0) creep.memory.upgrading = false;
-        
+        if (creep.carry.energy == 0) creep.memory.harvesting = true;
+        if (creep.carry.energy == creep.carryCapacity) creep.memory.harvesting = false;
+
+        //find targets to transfer energy to
         var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_SPAWN ||
@@ -12,35 +14,30 @@ var roleHarvester1 = {
                     }
         });
         
-	    if(creep.carry.energy < creep.carryCapacity && creep.memory.upgrading == false) {
+        //harvest energy
+	    if(creep.carry.energy < creep.carryCapacity && creep.memory.harvesting == true) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0].pos);
             }
         }
+        //move to target and transfer energy
         else if(targets.length > 0) {
             if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
             }
         }
+        //otherwise upgrade controller
         else {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
                 
-                if (creep.memory.upgrading == false) {
+                if (creep.memory.harvesting == true) {
                     creep.say("upgrading")
-                    creep.memory.upgrading = true;
+                    creep.memory.harvesting = false;
                 }
             }
         }
-        /*
-        if (creep.memory.upgrading = true) {
-            creep.upgradeController(creep.room.controller);
-            if (creep.carry.energy == 0) creep.memory.upgrading = false;
-            
-        }
-        */
-        
 	}
 };
 
--- code/role.harvester2.js
+++ code/role.harvester2.js
@@ -38,38 +38,43 @@ var roleHarvester2 = {
 
     run: function(creep) {
         
-        if (creep.carry.energy == 0) creep.memory.upgrading = false;
-        
+        if (creep.carry.energy == 0) creep.memory.harvesting = true;
+        if (creep.carry.energy == creep.carryCapacity) creep.memory.harvesting = false;
+
+        //find targets to transfer energy to
         var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
+                        return (structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                     }
         });
         
-        if(creep.carry.energy < creep.carryCapacity && creep.memory.upgrading == false) {
+        //harvest energy
+        if(creep.carry.energy < creep.carryCapacity && creep.memory.harvesting == true) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1].pos);
             }
         }
+        //move to target and transfer energy
         else if(targets.length > 0) {
             if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
             }
         }
+        //otherwise upgrade controller
         else {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
                 
-                if (creep.memory.upgrading == false) {
+                if (creep.memory.harvesting == true) {
                     creep.say("upgrading")
-                    creep.memory.upgrading = true;
+                    creep.memory.harvesting = false;
                 }
             }
-        }        
-	}
+        }
+    }
 };
 
 module.exports = roleHarvester2;
