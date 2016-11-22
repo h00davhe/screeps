4e320d0bed0a74aeeb8d2c0e93bf9b97d9daf4e1
Zyrax <h00davhe@gmail.com>
2016-11-22 18:20:46 +0100

    harvester reset

--- code/main.js
+++ code/main.js
@@ -36,11 +36,11 @@ module.exports.loop = function () {
     }
  */
      if(harvester1.length < 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'harvester1'});
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'harvester1'});
         if (!(newName < 0)) console.log('Spawning new harvester1: ' + newName);
     }
     if(harvester2.length < 2) {
-        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'harvester2'});
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'harvester2'});
         if (!(newName < 0)) console.log('Spawning new harvester2: ' + newName);
     } 
     if(upgrader1.length < 4) {
