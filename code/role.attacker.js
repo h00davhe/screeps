59c9154748f262223c3ba6e56b8167456e355ad7
Zyrax <h00davhe@gmail.com>
2016-11-23 23:41:17 +0100

    attacker

--- code/role.attacker.js
+++ code/role.attacker.js
@@ -20,19 +20,20 @@ module.exports = {
 */
             //just rush spawn
             var targets = creep.room.find(FIND_HOSTILE_SPAWNS);
+            var target;
             if(targets.length) {
                 if(creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
-            else if((var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);)) {
+            else if(target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS)) {
                 //kill creeps
                 if(creep.attack(target) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
                 }
             }
             else {
-                let target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
+                target = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES);
                 if(target) {
                     if(creep.attack(target) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(target);
