60176d788c74b73f39a1eab7329d26a9c4806cb2
Zyrax <h00davhe@gmail.com>
2016-11-24 19:03:34 +0100

    #creeps

--- code/main.js
+++ code/main.js
@@ -66,7 +66,7 @@ module.exports.loop = function () {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'upgrader2'});
         if (!(newName < 0)) console.log('Spawning new upgrader2: ' + newName);
     }
-    if(builder.length < 2 && harvester1.length > 1 && harvester2.length > 1) {
+    if(builder.length < 1 && harvester1.length > 1 && harvester2.length > 1) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: 'builder', repairMostDamagedFirst: false});
         if (!(newName < 0)) console.log('Spawning new builder: ' + newName);        
     }
